18d401849063207735593d9d8224af472de3596b
Souleymane Diallo <souleymanetimbidiallo@gmail.com>
2025-04-06 21:47:33 +0200

    Déploiement Angular vers GitHub Pages

--- docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-VWSVXZ4J.mjs";import"./chunk-66WLBMGS.mjs";import"./chunk-MI7AC25W.mjs";import"./chunk-BBGEAJNI.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
