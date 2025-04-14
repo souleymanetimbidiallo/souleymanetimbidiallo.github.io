8aac770861e748fe719cd53db90497548b444d06
Souleymane Diallo <souleymanetimbidiallo@gmail.com>
2025-04-14 23:28:08 +0200

    Nouvelle version Angular avec déploiement auto

---
+++ move-dist.js
@@ -0,0 +1,17 @@
+// move-dist.js
+const fs = require('fs');
+const path = require('path');
+
+const source = path.join(__dirname, 'docs', 'browser');
+const destination = path.join(__dirname, 'docs');
+
+// Lire tous les fichiers du dossier browser
+fs.readdirSync(source).forEach(file => {
+  const srcPath = path.join(source, file);
+  const destPath = path.join(destination, file);
+  fs.renameSync(srcPath, destPath); // déplace chaque fichier
+});
+
+// Supprimer le dossier browser
+fs.rmdirSync(source, { recursive: true });
+console.log('✅ Dossier browser déplacé avec succès dans docs/');
